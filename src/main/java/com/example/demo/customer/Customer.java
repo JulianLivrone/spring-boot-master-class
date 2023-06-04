06cfb3f4b4abfe745c315eb1df55f6446e7fc6e0
julian livrone <julian.livrone@gmail.com>
2023-06-04 17:16:20 -0300

    Added some changes related to Jackson library

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -1,20 +1,31 @@
 package com.example.demo.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
     private final Long id;
     private final String name;
+    private final String password;
 
-    public Customer(Long id, String name) {
+    public Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
+    @JsonProperty("customer_id") // It allows us to change the name of the property when we return it to the client (before was id and now customer_id)
     public Long getId() {
         return id;
     }
 
-    public String getName() {
+    public String getName() { // Jackson uses the getters in order to know what to return back to the client and with which name and value
         return name;
+    }
+
+    @JsonIgnore // So the application doesn't return the password in the JSON object back to the client.
+    public String getPassword() {
+        return password;
     }
 
     @Override
@@ -22,6 +33,7 @@ public class Customer {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -1,8 +1,5 @@
 package com.example.demo.customer;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,8 +10,8 @@ public class CustomerFakeRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         return Arrays.asList(
-                new Customer(1L, "James Bond"),
-                new Customer(2L, "Aladin")
+                new Customer(1L, "James Bond", "password123"),
+                new Customer(2L, "Aladin", "123password")
         );
     }
 }
