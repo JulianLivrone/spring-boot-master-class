6e1d34e94bf96ff1fc65eee5999dba9b23f80de0
julian livrone <julian.livrone@gmail.com>
2023-06-04 16:22:28 -0300

    Added classes needed to make N Tier Architecture and connect them with Dependency Injection

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,13 +1,22 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
 
-    @GetMapping
-    public Customer getCustomer(){
-        return new Customer(1L, "James Bond");
+    private final CustomerService customerService;
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/")
+    List<Customer> getCustomer(){
+        return customerService.getCustomer();
     }
 }
--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,4 +1,20 @@
 package com.example.demo.customer;
 
-public class CustomerRepository {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component // Makes this class a Bean, this means that the Spring environment will know this class and will be able to inject it into other classes (Dependency Injection)
+@Primary // Makes this class the primary implementation of an interface, so @Autowired will automatically know which implementation to inject into the CustomerService attribute "customerRepo"
+public class CustomerRepository implements CustomerRepo {
+    @Override
+    public List<Customer> getCustomers() {
+//      ToDo connect to real db
+        return Collections.singletonList(
+                new Customer(1L, "TODO. Implement real db")
+        );
+    }
 }
