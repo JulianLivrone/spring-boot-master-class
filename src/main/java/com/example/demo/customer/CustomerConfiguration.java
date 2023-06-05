090ad4e98e38a4a228a2cf03bb391a12e5001f60
julian livrone <julian.livrone@gmail.com>
2023-06-05 17:29:17 -0300

    Added more application.properties depending on the environment and used some properties and environment variables

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,21 +1,33 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
 
-    @Value("${app.useFakeCustomerRepo:false}")
+    @Value("${app.useFakeCustomerRepo:false}") // This value comes from the program arguments in configurations when we run DemoApplication
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.app.name}") // This value comes from application.properties
+    private String appName;
+
+    @Autowired
+    private Environment environment; // This is not the recommended way to do it, the recommended way is to inject it in the constructor as we have already seen
+
     @Bean // Allows us to instantiate a class where we can have extra configuration and setup steps before the class is initialized. Useful when we want to initialize a class and have some setup or configure before initialization.
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp) { // We inject the configuration app in the commandLineRunner to execute some code using these environment variables.
         // We use this class when we want to do some setup when the application starts, and we can inject services or classes that we have and maybe add something to the database.
         return args -> {
             System.out.println("Command line runner hooray");
+            System.out.println(appName);
+            System.out.println(environment.getProperty("info.app.description"));
+            System.out.println(infoApp);
         };
     }
 
